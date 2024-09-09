7abd90f7269c2cc7fb8cad5c57bba94dfe7fe920
Efan Tift <efantift@Efans-Air.attlocal.net>
2024-09-09 16:24:34 -0700

    Fixed duplicate guesses and made it print out pretty, also swapped to a better dictionary more in line with NYT. Works good!

--- LBSolver.py
+++ LBSolver.py
@@ -1,14 +1,20 @@
 from english_words import get_english_words_set
 import string
-web2lowerset = get_english_words_set(['web2'], lower=False, alpha=True)
-wordlist = list(web2lowerset)
+#web2lowerset = get_english_words_set(['web2'], lower=False, alpha=True)
+#wordlist = list(web2lowerset)
+
+with open('words_alpha.txt', 'r') as file:
+    
+    wordlist = [line.strip() for line in file]
+
 wordlist = [word for word in wordlist if not word[0].isupper()]
 
-print(wordlist[0:100])
-print(len(wordlist))
+
+
 wordlist = [word for word in wordlist if len(word) >= 3]
-print(wordlist[0:100])
-print(len(wordlist))
+
+
+
 
 alphabet = set(string.ascii_lowercase)
 userletters = input("Enter 12 unique letters separated by spaces, starting with the top left letter and continuing clockwise: ").split()
@@ -16,16 +22,16 @@ if len(userletters) != 12 or len(set(userletters)) != 12:
         print("Please enter exactly 12 unique letters.")
     
 groups = [userletters[i:i+3] for i in range(0, len(userletters), 3)]
-print(groups)
+
 
 
 
 userset = set(userletters)
 badletters = alphabet-userset
-print(badletters)
+
 wordlist = [word for word in wordlist if not badletters.intersection(set(word))]
-print(len(wordlist))
-print(wordlist[0:100])
+
+
 goodwords = []
 for word in wordlist:
         for index, letter in enumerate(word):
@@ -35,28 +41,33 @@ for word in wordlist:
                         if (word[index + 1] == word[index]):
                                 break
                         
-print(len(goodwords))
-print(goodwords[0:100])
-pogwords = []
-print(groups[1])
+
+allowedwords = []
+
 for word in goodwords:
         for index, letter in enumerate(word):
                 if (index == (len(word)-1)):
-                        pogwords.append(word)
+                        allowedwords.append(word)
                 else:
                         if (((word[index] in groups[0]) and (word[index+1] in groups[0])) or ((word[index] in groups[1]) and (word[index+1] in groups[1])) or ((word[index] in groups[2]) and (word[index+1] in groups[2])) or ((word[index] in groups[3]) and (word[index+1] in groups[3]))):
                                 break
 
-print(len(pogwords))
-print(pogwords[0:100])
-
-allowedwords = []
+validguesses = {'ZORA IS AWESOME'}
 
 for word in allowedwords:
         remainingletters = userset-set(word)
         for guess in allowedwords:
-                if ((guess.intersection(remainingletters) == remainingletters) and (guess[-1] == word[0]) or (guess.intersection(remainingletters) == remainingletters) and (word[-1] == guess[0])) 
+                if (set(guess).intersection(remainingletters) == remainingletters) and (guess[-1] == word[0]):
+                        validguesses.add((guess, word))
+                elif (set(guess).intersection(remainingletters) == remainingletters) and (word[-1] == guess[0]): 
+                        validguesses.add((word, guess))
+                        
 
 
+for items in validguesses:
+        print(items)
+        print("----------------------------------")
 
-secondword = [] 
+print(len(validguesses))
+
+
