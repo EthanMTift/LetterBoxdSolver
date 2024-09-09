afd5ef69bdc58369dcde352d195466b72e777e32
Zora LM <zlehmermearns9702@sdsu.com>
2024-09-09 14:21:40 -0700

    I added a loop that removes proper nouns from wordlist

--- LBSolver.py
+++ LBSolver.py
@@ -1,6 +1,9 @@
 from english_words import get_english_words_set
 web2lowerset = get_english_words_set(['web2'], lower=False, alpha=True)
 wordlist = list(web2lowerset)
+wordlist = [word for word in wordlist if not word[0].isupper()]
+
 print(wordlist[0:100])
 print(len(wordlist))
+
 
