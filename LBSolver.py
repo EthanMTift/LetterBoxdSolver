74a8be681aa53ed1c3361fe5c7efddb52ca80797
Efan Tift <efantift@Efans-Air.attlocal.net>
2024-09-09 14:36:15 -0700

    Added length functionality and sorting out of the bad letters

--- LBSolver.py
+++ LBSolver.py
@@ -1,6 +1,23 @@
 from english_words import get_english_words_set
+import string
 web2lowerset = get_english_words_set(['web2'], lower=False, alpha=True)
 wordlist = list(web2lowerset)
 print(wordlist[0:100])
 print(len(wordlist))
+wordlist = [word for word in wordlist if len(word) >= 3]
+print(wordlist[0:100])
+print(len(wordlist))
+
+alphabet = set(string.ascii_lowercase)
+userletters = input("Enter 12 unique letters separated by spaces: ").split()
+if len(userletters) != 12 or len(set(userletters)) != 12:
+    print("Please enter exactly 12 unique letters.")
+    
+
+userset = set(userletters)
+badletters = alphabet-userset
+print(badletters)
+wordlist = [word for word in wordlist if not badletters.intersection(set(word))]
+print(len(wordlist))
+print(wordlist[0:100])
 
