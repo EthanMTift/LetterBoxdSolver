19a2cd4f2a7461758477f4cd0ee5c82a70a9e3d8
Zora LM <zlehmermearns9702@sdsu.com>
2024-09-09 14:54:29 -0700

    spliced the userletters into 4 groups of 3

--- LBSolver.py
+++ LBSolver.py
@@ -11,10 +11,14 @@ print(wordlist[0:100])
 print(len(wordlist))
 
 alphabet = set(string.ascii_lowercase)
-userletters = input("Enter 12 unique letters separated by spaces: ").split()
+userletters = input("Enter 12 unique letters separated by spaces, starting with the top left letter and continuing clockwise: ").split()
 if len(userletters) != 12 or len(set(userletters)) != 12:
     print("Please enter exactly 12 unique letters.")
     
+groups = [userletters[i:i+3] for i in range(0, len(userletters), 3)]
+print(groups)
+
+
 
 userset = set(userletters)
 badletters = alphabet-userset
